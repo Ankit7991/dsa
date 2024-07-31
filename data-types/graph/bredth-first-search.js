bd45749f59516f16c90b97aeacd6ef30e48bffd2
ankitevince <ankit+github@evincedev.com>
2024-07-31 12:35:15 +0530

    working djkstra for requirement

--- data-types/graph/bredth-first-search.js
+++ data-types/graph/bredth-first-search.js
@@ -1,10 +1,12 @@
 
 class Graph {
 	constructor(edges = [], verticesCount = 0) {
-		this.graph = {};
+		this.verticesMap = {};
+		this.verticesCount = verticesCount;
 
 		for(let i = 0; i < verticesCount; i++) {
-			this.graph[i] = [];
+			// this.verticesMap[i] = [];
+			this.verticesMap[i] = {};
 		}
 		edges.map(edge => {
 			this.addEdge(...edge);
@@ -12,20 +14,21 @@ class Graph {
 	}
 
 	addEdge(vertex1, vertex2, weight) {
-		this.graph[vertex1].push([vertex2, weight]);
+		// this.verticesMap[vertex1].push([vertex2, weight]);
+		this.verticesMap[vertex1][vertex2] = weight;
 		// for undirected graphs
-		this.graph[vertex2].push([vertex1, weight]);
+		// this.verticesMap[vertex2].push([vertex1, weight]);
+		this.verticesMap[vertex2][vertex1] = weight;
 	}
 
-	bfs(start, distanceThreshold) {
+	bfs(start) {
 		const queue = [[start, 0]];
 		const visited = new Set([start]);
 		let index = 0;
 
 		while(index < queue.length) {
-			console.log(queue[ index ]);
 			const [vertex, weight] = queue[index++];
-			let vertices = this.graph[ vertex ];
+			let vertices = Object.entries(this.verticesMap[ vertex ]);
 
 			vertices.forEach(el => {
 				if(!visited.has(el[0])) queue.push(el);
@@ -35,6 +38,42 @@ class Graph {
 		}
 
 		return [...visited];
+	}
+
+	djkstra(start, distanceThreshold) {
+		/* index: toCity, value[fromCity, distance] */
+		const distanceList = Array.from({ length: this.verticesCount }, () => [ start, Infinity ]);
+		const visited = new Set([start]);
+		const queue = [[start, start]];
+		distanceList[start][1] = 0;
+		let index = 0;
+		
+		while (queue.length > index) {
+			const [fromCity, currentCity] = queue[index++];
+			const neighbours = this.verticesMap[currentCity];
+			
+			visited.add(currentCity);
+
+			const [$fromCity, distance] = distanceList[currentCity];
+
+			let distanceFromMap = neighbours[fromCity] || Infinity;
+
+			if(distanceFromMap < distance) {
+				distanceList[ currentCity ] = [ fromCity, distanceFromMap ];
+			}
+			
+			for(let nVertex in neighbours) {
+				nVertex = parseInt(nVertex);
+				if(!visited.has(nVertex)) queue.push([currentCity, nVertex]);
+			}
+
+			// neighbours.map(([nVertex, nWeight]) => {
+			// 	if(!visited.has(nVertex)) queue.push(nVertex);
+			// })
+
+		}
+
+		console.log(distanceList);
 	}
 }
 
@@ -50,8 +89,11 @@ class Graph {
 var findTheCity = function (n, edges, distanceThreshold) {
 	const graph = new Graph(edges, n);
 	let bfs1 = graph.bfs(1, distanceThreshold);
+	graph.djkstra(0, distanceThreshold);
 	console.log(bfs1);
 };
 
 
-findTheCity(6, [ [ 0, 1, 10 ], [ 0, 2, 1 ], [ 2, 3, 1 ], [ 1, 3, 1 ], [ 1, 4, 1 ], [ 4, 5, 10 ] ], 20);
+// findTheCity(6, [ [ 0, 1, 10 ], [ 0, 2, 1 ], [ 2, 3, 1 ], [ 1, 3, 1 ], [ 1, 4, 1 ], [ 4, 5, 10 ] ], 20);
+// findTheCity(4, [[0, 1, 2], [1, 2, 3], [1, 3, 4], [2, 3, 4]], 6);
+findTheCity(6, [[0,1,3], [0,2,2], [1,3,4], [2,3,3], [3,4,5]], 10);
